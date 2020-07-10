a5c33639a68594ac74c0f61334a623f36a8fe84c
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-07-10 17:07:36 -0400

    ex 3.1

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,18 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require("request-promise");
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return request("http://api.open-notify.org/iss-now.json").then((response) => {
+    const parsedResponse = JSON.parse(response);
+    let lat = parsedResponse.iss_position.latitude;
+    let lng = parsedResponse.iss_position.longitude;
+    let coordinates = { lat, lng };
+    return coordinates;
+  });
 };
+
+//console.log(getIssPosition());
+getIssPosition().then((result) => console.log(result));
