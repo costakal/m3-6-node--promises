50b1b61af4df35aba1ea638b5d748e033e2309bd
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-07-10 13:20:56 -0400

    exercise 1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,23 +1,47 @@
 // Exercise 1
 // ------------
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
-const complicatedArray = ['cucumber', 44, true];
+const arrayOfWords = ["cucumber", "tomatos", "avocado"];
+const complicatedArray = ["cucumber", 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  firstPromise = new Promise((resolve, reject) => {
+    if (
+      array.every((word) => {
+        return typeof word === "string";
+      })
+    ) {
+      const turnUpperCaps = array.map((item) => {
+        console.log(item, item.toUpperCase());
+        return item.toUpperCase();
+      });
+      resolve(turnUpperCaps);
+    } else {
+      reject("This will not work");
+    }
+  });
+  return firstPromise;
 };
 
 const sortWords = (array) => {
-  // write some code
+  secondPromise = new Promise((resolve, reject) => {
+    const sortWords = array.sort();
+    resolve(sortWords);
+    reject("This will not work");
+  });
+  return secondPromise;
 };
 
 // Calling (testing)
 makeAllCaps(arrayOfWords)
   .then(sortWords)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+  .then((result) => console.log("This is the result of good array", result))
+  .catch((error) => console.log("This is the error of good array", error));
 
 makeAllCaps(complicatedArray)
   .then(sortWords)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+  .then((result) =>
+    console.log("This is the result of complicating array", result)
+  )
+  .catch((error) =>
+    console.log("This is the error of complicating array", error)
+  );
